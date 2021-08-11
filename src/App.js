1e506470c6fd530e41cb5d8c86855588585053e9
mhovey <mark.hovey@smartsheet.com>
2021-08-11 14:09:05 -0700

    creating tests for app and checkbox

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
 import React, { useState, useEffect, useReducer } from "react";
 import './App.css';
 
-// https://api.github.com/users/markhoveyjr
 
 function App({ login }) {
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        if(!login) return;
-        setLoading(true);
-        fetch(`https://api.github.com/users/${login}`)
-            .then((response) => response.json())
-            .then(setData)
-            .then(() => setLoading(false))
-            .catch(setError);
-    }, [login]);
-
-    if (loading) return <h1>Loading...</h1>;
-    if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
-    if (!data) return null;
 
         return (
             <div>
-            <h1>{data.login}</h1>
-            <p>{data.location}</p>
-            <img alt={data.login} src={data.avatar_url}/>
+                    <h1>Hello React Testing Library!</h1>
         </div>
         );
 }
