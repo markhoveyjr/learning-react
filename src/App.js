4ff4522b4583ba0bdde4a54deeeb4173f18bda21
mhovey <mark.hovey@smartsheet.com>
2021-08-11 09:45:07 -0700

    app to get and display github user info via api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useReducer } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// https://api.github.com/users/markhoveyjr
+
+function App({ login }) {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        if(!login) return;
+        setLoading(true);
+        fetch(`https://api.github.com/users/${login}`)
+            .then((response) => response.json())
+            .then(setData)
+            .then(() => setLoading(false))
+            .catch(setError);
+    }, [login]);
+
+    if (loading) return <h1>Loading...</h1>;
+    if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
+    if (!data) return null;
+
+        return (
+            <div>
+            <h1>{data.login}</h1>
+            <p>{data.location}</p>
+            <img alt={data.login} src={data.avatar_url}/>
+        </div>
+        );
 }
 
 export default App;
